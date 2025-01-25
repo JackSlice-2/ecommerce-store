8c9ebca6ade218d4399e94fa0c9cf748af41d4c1
JackSlice2 <jackslice9@gmail.com>
2025-01-25 19:04:06 -0300

    /cart is now working

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -1,55 +1,21 @@
-import Image from "next/image";
-import { X } from "lucide-react";
+'use client'
 
-import IconButton from "@/components/ui/icon-button";
-import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
-import { Product } from "@/types";
+import CartItemComponent from "./CartItemComponent";
 
-
-interface CartItemProps {
-  data: Product;
-}
-
-const CartItem: React.FC<CartItemProps> = ({
-  data
-}) => {
+const CartItem = () => {
   const cart = useCart();
-
-  const onRemove = () => {
-    cart.removeItem(data.id);
-  };
-
-  return ( 
-    <li className="flex py-6 border-b">
-      <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
-        <Image
-          fill
-          src={data.images[0].url}
-          alt=""
-          className="object-cover object-center"
-        />
-      </div>
-      <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-        <div className="absolute z-10 right-0 top-0">
-          <IconButton onClick={onRemove} icon={<X size={15} />} />
-        </div>
-        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-          <div className="flex justify-between">
-            <p className=" text-lg font-semibold text-black dark:text-white">
-              {data.name}
-            </p>
-          </div>
-
-          <div className="mt-1 flex text-sm">
-            <p className="text-gray-500 dark:text-white">{data.color.name}</p>
-            <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500 dark:text-white">{data.size.name}</p>
-          </div>
-          <Currency value={data.price} />
-        </div>
-      </div>
-    </li>
-  );
+  
+  return (
+    <div className="lg:col-span-7">
+      {cart.items.length === 0 && <p className="text-neutral-500 dark:text-white">No items added to cart.</p>}
+      <ul>
+        {cart.items.map((item) => (
+          <CartItemComponent key={item.id} data={item} />
+        ))}
+      </ul>
+    </div>
+  )
 }
  
 export default CartItem;
